bad51bd6c37bc299814c4f68d62b24e7127c7771
Marek Aufart <maufart@redhat.com>
2015-03-26 10:52:56 +0100

    Add overcloud image create method
    Adding overcloud image build method to replace instack
    code and do deployment via rdomanager commands.
    
    Change-Id: Icc39744c511013d663242f5b544b0ac4a2fd6893

--- rdomanager_oscplugin/tests/v1/overcloud_image/test_overcloud_image.py
+++ rdomanager_oscplugin/tests/v1/overcloud_image/test_overcloud_image.py
@@ -13,6 +13,8 @@
 #   under the License.
 #
 
+import mock
+
 from rdomanager_oscplugin.tests.v1.test_plugin import TestPluginV1
 
 # Load the plugin init module for the plugin list and show commands
@@ -40,3 +42,58 @@ class TestOvercloudImageCreate(TestPluginV1):
 
         # Get the command object to test
         self.cmd = overcloud_image.CreatePlugin(self.app, None)
+
+        self.app.client_manager.image = mock.Mock()
+        self.app.client_manager.compute = mock.Mock()
+        self.cmd._read_image_file = mock.Mock(return_value=b'IMGDATA')
+
+    @mock.patch('os.path.isfile', return_value=True)
+    @mock.patch('subprocess.call')
+    def test_overcloud_create_images(self, mock_subprocess_call, mock_isfile):
+        parsed_args = self.check_parser(self.cmd, [], [])
+
+        self.cmd.take_action(parsed_args)
+
+        self.assertEqual(mock_isfile.call_count, 5)
+        self.assertEqual(
+            mock_isfile.call_args_list, [
+                mock.call('./deploy-ramdisk-ironic.initramfs'),
+                mock.call('./deploy-ramdisk-ironic.kernel'),
+                mock.call('./discovery-ramdisk.initramfs'),
+                mock.call('./discovery-ramdisk.kernel'),
+                mock.call('./openstack-full.qcow2')
+            ])
+
+        self.assertEqual(
+            self.app.client_manager.image.images.delete.call_count,
+            2
+        )
+        self.assertEqual(
+            self.app.client_manager.image.images.create.call_count,
+            2
+        )
+        self.assertEqual(
+            self.app.client_manager.image.images.create.call_args_list,
+            [mock.call(disk_format='aki',
+                       name='bm_deploy_kernel',
+                       data=b'IMGDATA',
+                       is_public=True),
+             mock.call(disk_format='ari',
+                       name='bm_deploy_ramdisk',
+                       data=b'IMGDATA',
+                       is_public=True)])
+
+        self.assertEqual(
+            self.app.client_manager.compute.flavors.find.call_count,
+            1
+        )
+
+        self.assertEqual(mock_subprocess_call.call_count, 3)
+        self.assertEqual(
+            mock_subprocess_call.call_args_list, [
+                mock.call('tripleo load-image -d ./openstack-full.qcow2'),
+                mock.call('sudo cp -f "./discovery-ramdisk.kernel" '
+                          '"/tftpboot/discovery.kernel"'),
+                mock.call('sudo cp -f "./discovery-ramdisk.initramfs" '
+                          '"/tftpboot/discovery.ramdisk"')
+            ])
