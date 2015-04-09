9ef861899b8e31d1e7102362a07608aadee5dcf8
Dougal Matthews <dougal@redhat.com>
2015-04-09 08:18:03 +0100

    WIP
    Change-Id: Icad3d15d67b59683041f19f82e3a8ce528720e0a

--- rdomanager_oscplugin/v1/overcloud_deploy.py
+++ rdomanager_oscplugin/v1/overcloud_deploy.py
@@ -15,18 +15,74 @@
 
 import logging
 
+from rdomanager_oscplugin.v1 import util
+
 from cliff import command
+
+
+CONFIG = {
+    'NeutronPublicInterface': 'nic1',
+    'HypervisorNeutronPublicInterface': 'nic1',
+    'NEUTRON_NETWORK_TYPE': 'gre',
+    'NEUTRON_TUNNEL_TYPES': 'gre',
+    'OVERCLOUD_LIBVIRT_TYPE': 'qemu',
+    'NtpServer': '',
+    'OVERCLOUD_EXTRA_CONFIG': '{}',
+
+    'CONTROLSCALE': 1,
+    'COMPUTESCALE': 1,
+    'CEPHSTORAGESCALE': 0,
+    'BLOCKSTORAGESCALE': 0,
+    'SWIFTSTORAGESCALE': 0,
+
+    'OVERCLOUD_CONTROLLER_IMAGE': 'overcloud-full',
+    'OVERCLOUD_COMPUTE_IMAGE': 'overcloud-full',
+    'OVERCLOUD_BLOCKSTORAGE_IMAGE': 'overcloud-full',
+    'OVERCLOUD_SWIFTSTORAGE_IMAGE': 'overcloud-full',
+    'OVERCLOUD_CEPHSTORAGE_IMAGE': 'overcloud-full',
+
+    'OVERCLOUD_CONTROL_FLAVOR': "baremetal_control",
+    'OVERCLOUD_COMPUTE_FLAVOR': "baremetal_compute",
+    'OVERCLOUD_CEPHSTORAGE_FLAVOR': "baremetal_ceph-storage",
+
+    'OVERCLOUD_BLOCKSTORAGE_FLAVOR': "baremetal_compute",
+    'OVERCLOUD_SWIFTSTORAGE_FLAVOR': "baremetal_compute",
+}
 
 
 class DeployPlugin(command.Command):
     """Overcloud Image Build plugin"""
 
-    auth_required = False
     log = logging.getLogger(__name__ + ".BuildPlugin")
 
     def get_parser(self, prog_name):
         parser = super(DeployPlugin, self).get_parser(prog_name)
+        parser.add_argument('-P', '--parameter', dest='parameters',
+                            metavar='<KEY1=VALUE1>',
+                            help='This can be specified multiple times.',
+                            action='append')
+        parser.add_argument('-t', '--template')
+        parser.add_argument('-r', '--resource-registry')
         return parser
 
     def take_action(self, parsed_args):
         self.log.debug("take_action(%s)" % parsed_args)
+
+        # Parameters
+        # - Paramaters - scale counts and flavors
+        # - template (optional)
+        # - resource registry (optional)
+
+        util.wait_for_hypervisor_stats(self.app.client_manager.compute)
+
+        util.generate_overcloud_passwords()
+
+        network_client = self.app.client_manager.network
+        net = network_client.api.find_attr(
+            'networks',
+            parsed_args.identifier,
+        )
+        print net
+        print dir(net)
+
+        # CONFIG['NeutronControlPlaneID'] = self.app.client_manager.network.
