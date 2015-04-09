710d40cf3a62dc7a8f5c4feb23f9764f55a07e2c
Dougal Matthews <dougal@redhat.com>
2015-04-09 08:18:03 +0100

    WIP
    Change-Id: Icad3d15d67b59683041f19f82e3a8ce528720e0a

--- rdomanager_oscplugin/v1/overcloud_deploy.py
+++ rdomanager_oscplugin/v1/overcloud_deploy.py
@@ -15,7 +15,20 @@
 
 import logging
 
+from rdomanager_oscplugin.v1 import util
+
 from cliff import command
+
+
+CONFIG = {
+    'NeutronPublicInterface': 'nic1',
+    'HypervisorNeutronPublicInterface': 'nic1',
+    'NEUTRON_NETWORK_TYPE': 'gre',
+    'NEUTRON_TUNNEL_TYPES': 'gre',
+    'OVERCLOUD_LIBVIRT_TYPE': 'qemu',
+    'NtpServer': '',
+    'OVERCLOUD_EXTRA_CONFIG': '{}'
+}
 
 
 class DeployPlugin(command.Command):
@@ -26,7 +39,22 @@ class DeployPlugin(command.Command):
 
     def get_parser(self, prog_name):
         parser = super(DeployPlugin, self).get_parser(prog_name)
+        parser.add_argument('-P', '--parameter', dest='parameters',
+                            metavar='<KEY1=VALUE1>',
+                            help='This can be specified multiple times.',
+                            action='append')
+        parser.add_argument('-t', '--template')
+        parser.add_argument('-r', '--resource-registry')
         return parser
 
     def take_action(self, parsed_args):
         self.log.debug("take_action(%s)" % parsed_args)
+
+        # Parameters
+        # - Paramaters - scale counts and flavors
+        # - template (optional)
+        # - resource registry (optional)
+
+        util.generate_overcloud_passwords()
+
+        util.wait_for_hypervisor_stats()
