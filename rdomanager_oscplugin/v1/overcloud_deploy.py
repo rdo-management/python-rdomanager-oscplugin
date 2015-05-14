1444ebeaca88492c0280a9b39c9df66ebaf1e9fe
Dougal Matthews <dougal@redhat.com>
2015-05-14 17:47:03 +0100

    Rename overcloud deploy command
    Change-Id: I5c4517f8eab75a20916eac26fb637dbaa9d2b840

--- rdomanager_oscplugin/v1/overcloud_deploy.py
+++ rdomanager_oscplugin/v1/overcloud_deploy.py
@@ -14,18 +14,65 @@
 #
 
 import logging
+import os
 
 from cliff import command
 
 
-class DeployPlugin(command.Command):
-    """Overcloud Image Build plugin"""
+TRIPLEO_HEAT_TEMPLATES = "/usr/share/openstack-tripleo-heat-templates/"
+OVERCLOUD_YAML_PATH = os.path.join(TRIPLEO_HEAT_TEMPLATES,
+                                   "overcloud-without-mergepy.yaml")
+RESOURCE_REGISTRY_PATH = os.path.join(
+    TRIPLEO_HEAT_TEMPLATES, "overcloud-resource-registry-puppet.yaml")
 
-    auth_required = False
-    log = logging.getLogger(__name__ + ".BuildPlugin")
+PARAMETERS = {
+    'AdminPassword': None,
+    'AdminToken': None,
+    'CeilometerPassword': None,
+    'CeilometerMeteringSecret': None,
+    'CinderPassword': None,
+    'CinderISCSIHelper': 'lioadm',
+    'CloudName': 'overcloud',
+    'ExtraConfig': '{}',
+    'GlancePassword': None,
+    'HeatPassword': None,
+    'NeutronControlPlaneID': None,
+    'NeutronDnsmasqOptions': 'dhcp-option-force=26,1400',
+    'NeutronPassword': None,
+    'NeutronPublicInterface': 'nic1',
+    'NeutronFlatNetworks': 'datacentre',
+    'HypervisorNeutronPhysicalBridge': 'br-ex',
+    'NeutronBridgeMappings': 'datacentre:br-ex',
+    'HypervisorNeutronPublicInterface': 'nic1',
+    'NovaComputeLibvirtType': 'qemu',
+    'NovaPassword': None,
+    'SwiftHashSuffix': None,
+    'SwiftPassword': None,
+    'NeutronNetworkType': 'gre',
+    'NeutronTunnelTypes': 'gre',
+    'SnmpdReadonlyUserPassword': None,
+    'OvercloudControlFlavor': 'baremetal',
+    'OvercloudComputeFlavor': 'baremetal',
+    'OvercloudBlockStorageFlavor': 'baremetal',
+    'OvercloudSwiftStorageFlavor': 'baremetal',
+    'OvercloudCephStorageFlavor': 'baremetal',
+    'NtpServer': '',
+    'controllerImage': 'overcloud-full',
+    'NovaImage': 'overcloud-full',
+    'BlockStorageImage': 'overcloud-full',
+    'SwiftStorageImage': 'overcloud-full',
+    'CephStorageImage': 'overcloud-full',
+    'Debug': 'True',
+}
+
+
+class DeployOvercloud(command.Command):
+    """Deploy Overcloud"""
+
+    log = logging.getLogger(__name__ + ".DeployOvercloud")
 
     def get_parser(self, prog_name):
-        parser = super(DeployPlugin, self).get_parser(prog_name)
+        parser = super(DeployOvercloud, self).get_parser(prog_name)
         return parser
 
     def take_action(self, parsed_args):
