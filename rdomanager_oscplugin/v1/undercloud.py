1f89b73334402ac8ddbddbabd2e11339bfaf69f7
Lennart Regebro <lregebro@redhat.com>
2015-06-17 09:54:08 -0400

    Moving stack parameter defaults to undercloud create
    We don't want to overwrite parameters when deploying unless explicitly given.
    This change moves the defaults to "undercloud create" and only sets
    parameters when given, enabling you to set Tuskar parameters through the GUI
    or with other CLI commands.
    
    See also: https://bugzilla.redhat.com/show_bug.cgi?id=1227873
    
    Change-Id: I48a4dabe0d1a7fcfb8c50f11a49f23efdef0908b

--- rdomanager_oscplugin/v1/undercloud.py
+++ rdomanager_oscplugin/v1/undercloud.py
@@ -19,6 +19,9 @@ import logging
 
 from cliff import command
 from instack_undercloud import undercloud
+from oslo_config import cfg
+from tuskarclient import client
+from tuskarclient.common import utils
 
 
 class InstallPlugin(command.Command):
@@ -32,5 +35,56 @@ class InstallPlugin(command.Command):
 
         # TODO(trown): Make the location of the instack root dir configurable
         undercloud.install('.')
+
+        tuskar_defaults = {
+            'Ceph-Storage-1::count': '0',
+            'Ceph-Storage-1::Flavor': 'baremetal',
+            'Ceph-Storage-1::Image': 'overcloud-full',
+            'Cinder-Storage-1::CinderISCSIHelper': 'lioadm',
+            'Cinder-Storage-1::count': '0',
+            'Cinder-Storage-1::Image': 'overcloud-full',
+            'Cinder-Storage-1::Flavor': 'baremetal',
+            'Compute-1::count': '1',
+            'Compute-1::Flavor': 'baremetal',
+            'Compute-1::Image': 'overcloud-full',
+            'Compute-1::NeutronBridgeMappings': 'datacentre:br-ex',
+            'Compute-1::NeutronEnableTunnelling': 'True',
+            'Compute-1::NeutronFlatNetworks': 'datacentre',
+            'Compute-1::NeutronNetworkType': 'gre',
+            'Compute-1::NeutronNetworkVLANRanges': 'datacentre:1:1000',
+            'Compute-1::NeutronPhysicalBridge': 'br-ex',
+            'Compute-1::NeutronPublicInterface': 'nic1',
+            'Compute-1::NeutronTunnelTypes': 'gre',
+            'Compute-1::NovaComputeLibvirtType': 'qemu',
+            'Compute-1::NtpServer': '',
+            'Controller-1::CinderISCSIHelper': 'lioadm',
+            'Controller-1::CloudName': 'overcloud',
+            'Controller-1::count': '1',
+            'Controller-1::Flavor': 'baremetal',
+            'Controller-1::Image': 'overcloud-full',
+            'Controller-1::NeutronBridgeMappings': 'datacentre:br-ex',
+            'Controller-1::NeutronEnableTunnelling': 'True',
+            'Controller-1::NeutronFlatNetworks': 'datacentre',
+            'Controller-1::NeutronNetworkType': 'gre',
+            'Controller-1::NeutronNetworkVLANRanges': 'datacentre:1:1000',
+            'Controller-1::NeutronPublicInterface': 'nic1',
+            'Controller-1::NeutronTunnelTypes': 'gre',
+            'Controller-1::NtpServer': '',
+            'Swift-Storage-1::count': '0',
+            'Swift-Storage-1::Flavor': 'baremetal',
+            'Swift-Storage-1::Image': 'overcloud-full',
+        }
+
+        management = client.get_client(
+            '2',
+            os_auth_token=cfg.CONF['auth']['undercloud_admin_token'],
+            os_tenant_name='admin',
+            tuskar_url='http://192.0.2.1:8585/v2')
+
+        plan = utils.find_resource(management.plans, 'overcloud')
+
+        parameters = [{'name': pair[0], 'value': pair[1]}
+                      for pair in tuskar_defaults.items()]
+        management.plans.patch(plan.uuid, parameters)
 
         return
