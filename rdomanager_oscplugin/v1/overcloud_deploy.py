3f01469a94bf6be7f2891f87931f6cfde3952454
marios <marios@redhat.com>
2015-07-07 16:28:02 +0300

    Wires up NeutronDhcpAgentsPerNetwork parameter to deploy
    The NeutronDhcpAgentsPerNetwork is a parameter in the tripleo
    heat templates and defaults to 2. This does not expose the
    parameter to the CLI but rather sets it to the number of
    controllers with a minimum of 3, as per
    https://github.com/beekhof/osp-ha-deploy/blob/master/pcmk/neutron-server.scenario#L133
    
    Depends-On: https://review.openstack.org/#/c/199102/
    Change-Id: Idc56324b187b191be6d900426225c8cbdc0eafd4

--- rdomanager_oscplugin/v1/overcloud_deploy.py
+++ rdomanager_oscplugin/v1/overcloud_deploy.py
@@ -278,11 +278,13 @@ class DeployOvercloud(command.Command):
                 parameters[param] = getattr(args, arg)
 
         # Scaling needs extra parameters
-        if parameters.get('Controller-1::count', 1) > 1:
+        number_controllers = parameters.get('Controller-1::count')
+        if number_controllers > 1:
             if args.use_tht:
                 parameters.update({
                     'NeutronL3HA': True,
                     'NeutronAllowL3AgentFailover': False,
+
                 })
             else:
                 parameters.update({
@@ -291,6 +293,20 @@ class DeployOvercloud(command.Command):
                     'Compute-1::NeutronL3HA': True,
                     'Compute-1::NeutronAllowL3AgentFailover': False,
                 })
+
+        # set at least 3 dhcp_agents_per_network
+        dhcp_agents_per_network = (number_controllers if number_controllers > 3
+                                   else 3)
+
+        if args.use_tht:
+            parameters.update({
+                'NeutronDhcpAgentsPerNetwork': dhcp_agents_per_network,
+            })
+        else:
+            parameters.update({
+                'Controller-1::NeutronDhcpAgentsPerNetwork':
+                    dhcp_agents_per_network,
+            })
 
         if parameters.get('Ceph-Storage-1::count', 0) > 0:
             parameters.update({
